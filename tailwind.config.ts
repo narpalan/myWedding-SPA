0145bc7df1d1edc8ea5bdabae9dad24493aee98b
athaaygomes <thaaygmacedo@gmail.com>
2024-08-25 02:34:23 -0300

    :sparkles: feat: add custom colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: { 
+      'mossGreen': '#9FB063',
+      'white':'#ffffff',
+      'violet':{
+        400: '#a78bfa',
+      },      
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
