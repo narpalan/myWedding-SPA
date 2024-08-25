9d74ca3449af77395a96977162c4eea7754952ce
athaaygomes <thaaygmacedo@gmail.com>
2024-08-25 01:37:31 -0300

    :lipstick: feat - change color bg body

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -6,7 +6,7 @@ export default function Wrapper({
     children: React.ReactNode
 }): React.JSX.Element {
     return (
-        <div className='col-span-4 lg:col-span-12'>
+        <div className='col-span-4 lg:col-span-12 bg-white'>
             {children}
         </div>
     );
