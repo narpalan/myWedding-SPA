c7b93528f28370f1a33642ad040323a1bbdae69c
narpalan <thiagofumega@gmail.com>
2024-09-29 21:29:12 -0300

    :lipstick: feat: adds gap y on top div, adds divider, fix margin and height, style text sizes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,41 @@ export default function Home() {
   return (
     <>      
       <Hero/>
+      <Divider id='couple' text="O Casal"/>  
       <Wrapper>
-          <div className='flex flex-col lg:flex-row justify-around my-28 mx-5'>              
+          <div className='flex flex-col h-[85rem] gap-y-16 lg:flex-row lg:justify-around my-5 mx-5 lg:mx-28 lg:h-[71.5vh]'>              
               <Image
                 src='/bg-candle-test.avif'
-                width={550}
-                height={910}
+                width={470}
+                height={20}
                 alt='Our photo wearing white robes!'                
               />
-              <div id='couple' className='flex flex-col lg:w-[877px] lg:h-[810px] sm:wrap '>
-                <h1>O Casal</h1>
-                <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+              <div className='flex flex-col lg:w-[650px] lg:h-[200vh]'>               
+                <span className='text-justify'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 
+                    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
+                    make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+                    typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus 
+                    PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting 
+                    industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer 
+                    took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
+                    centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised 
+                    in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
+                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy 
+                    text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since 
+                    the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has 
+                    survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+                    recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                   </span>
               </div>            
           </div>
       </Wrapper>
-      <Wrapper className='bg-violet-100'>
-        <div className='flex flex-row justify-around items-center text-black p-5'>
-          <span>Não se esqueça de confirmar sua presença até a data xx/xx/xxxx</span>
+      <Wrapper className='bg-violet-200 mt-56 lg:mt-5'>
+        <div className='flex flex-col gap-y-5 lg:flex-row lg:justify-around items-center text-black p-5'>
+          <span>Confirme sua presença até 30/11/2024</span>
           <a href='#rsvp'>
-            <button className="text-center rounded-3xl bg-mossGreen w-[16.31rem] h-[3.06rem]" type='submit'>
+            <button className="text-center rounded-3xl bg-mossGreen w-[12.31rem] h-[2.06rem] lg:w-[16.31rem] lg:h-[2.75rem]" type='submit'>
               <p>Cadastrar!</p>
             </button>
           </a>
