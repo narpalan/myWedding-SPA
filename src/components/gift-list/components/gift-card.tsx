503f62f513f94b68ae6608d8f35e4a49a47cee1f
narpalan <thiagofumega@gmail.com>
2024-10-09 00:47:31 -0300

    :recycle: refactor: add new img links/src and fix gift card height.

--- src/components/gift-list/components/gift-card.tsx
+++ src/components/gift-list/components/gift-card.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function GiftCard ({ src, title, description, link, key }: {src: string, title: string, description: string, link: string, key: number }): React.JSX.Element {
     return (
-    <div className='rounded-lg w-80 lg:w-80 flex flex-col px-10 lg:px-12 h-96 my-9 transform shadow-2xl pt-5 transition duration-300 hover:scale-105' key={key}>
+    <div className='rounded-lg w-80 lg:w-80 flex flex-col px-10 lg:px-12 h-[448px] my-9 transform shadow-2xl pt-5 transition duration-300 hover:scale-105' key={key}>
         <a href={link}>
             <Image
                 className='rounded'
