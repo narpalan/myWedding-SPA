d3297877243d6c0accf6ed24961e5bcf97822e7f
narpalan <thiagofumega@gmail.com>
2024-10-08 21:53:23 -0300

    :recycle: refactor: adds correct layout for mobile.

--- src/components/gift-list/gift-list.tsx
+++ src/components/gift-list/gift-list.tsx
@@ -13,7 +13,7 @@ export default function GiftList(): React.JSX.Element {
     return(
         <>
             <div className='flex flex-col gap-y-4 lg:flex-row lg:mx-28 lg:justify-between lg:my-16'>
-                <div className='w-full relative h-[189px] lg:w-[300px] md:h-[275px] lg:h-[450px]'>
+                <div className={`w-full relative h-[189px] lg:w-[300px] md:h-[275px] lg:h-[450px] ${isOpenA? 'border-4 border-purple-800': ''}`}>
                     <a 
                         href='#gifts-header-title'
                         onClick={() => {
@@ -28,9 +28,10 @@ export default function GiftList(): React.JSX.Element {
                         fill
                         className='object-cover'
                     /> 
-                    </a>   
-                </div>                            
-                <div className='w-full relative h-[189px] lg:w-[300px] md:h-[275px] lg:h-[450px]'>
+                    </a>                       
+                </div>
+                <List id='SM-A' className={`${isOpenA ? 'block': 'hidden'} lg:hidden`} section='A'/>                            
+                <div className={`w-full relative h-[189px] lg:w-[300px] md:h-[275px] lg:h-[450px] ${isOpenB? 'border-4 border-purple-800': ''}`}>
                 <a 
                     href='#gifts-header-title'
                     onClick={() => {
@@ -47,7 +48,8 @@ export default function GiftList(): React.JSX.Element {
                     />
                     </a>
                 </div>
-                <div className='w-full relative h-[189px] lg:w-[300px] md:h-[275px] lg:h-[450px]'>
+                <List id='SM-B' className={`${isOpenB ? 'block': 'hidden'} lg:hidden`} section='B'/>
+                <div className={`w-full relative h-[189px] lg:w-[300px] md:h-[275px] lg:h-[450px] ${isOpenC? 'border-4 border-purple-800': ''}`}>
                 <a 
                     href='#gifts-header-title'
                     onClick={() => {
@@ -63,18 +65,19 @@ export default function GiftList(): React.JSX.Element {
                         className='object-cover'                  
                     />  
                     </a>          
-                </div>
-                
+                </div> 
+                <List id='SM-C' className={`${isOpenC ? 'block': 'hidden'} lg:hidden`} section='C'/>                
             </div>
-            {(isOpenA || isOpenB || isOpenC) && (
+            
+            {/*(isOpenA || isOpenB || isOpenC) && (
                 <Divider id='gifts-header-title' text={headerTitle} />
-            )}
+            )*/}
 
-            <List className={`${isOpenA ? 'block': 'hidden'}`} section='A'/>
+            <List id='LG-A' className={`${isOpenA ? 'hidden lg:grid lg:grid-cols-3': 'hidden'}`} section='A'/>
                 
-            <List className={`${isOpenB ? 'block': 'hidden'}`} section='B'/>
+            <List id='LG-B' className={`${isOpenB ? 'hidden lg:grid lg:grid-cols-3': 'hidden'}`} section='B'/>
 
-            <List className={`${isOpenC ? 'block': 'hidden'}`} section='C'/>
+            <List id='LG-C' className={`${isOpenC ? 'hidden lg:grid lg:grid-cols-3': 'hidden'}`} section='C'/>
         </>
     );
 };
