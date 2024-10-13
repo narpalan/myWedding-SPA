011e19056155765fcc18251f5c5ccabd8425dc87
narpalan <thiagofumega@gmail.com>
2024-10-13 18:12:27 -0300

    :sparkles: feat: adds suggested value field.

--- src/components/gift-list/components/gift-card.tsx
+++ src/components/gift-list/components/gift-card.tsx
@@ -2,6 +2,9 @@
 import { useState } from 'react';
 import Image from 'next/image';
 
+
+const amazonListLink = 'https://www.amazon.com.br/hz/wishlist/ls/3Q8YYWX6OV1C1?ref_=wl_share';
+
 const useCopyToClipboard = () => {
     const [isCopied, setIsCopied] = useState(false);
 
@@ -17,13 +20,23 @@ const useCopyToClipboard = () => {
     return { isCopied, copyToClipboard };
 };
 
-export default function GiftCard ({ src, title, description, link, key }: {src: string, title: string, description: string, link: string, key: number }): React.JSX.Element {
-    const { isCopied, copyToClipboard } = useCopyToClipboard();
+export default function GiftCard ({ 
+    src, 
+    title, 
+    description, 
+    key,
+    price, }: {
+        src: string, 
+        title: string, 
+        description: string, 
+        key: number,
+        price: string, 
+    }): React.JSX.Element {
+    const { isCopied, copyToClipboard } = useCopyToClipboard();    
     return (
     
-    <div className='rounded-lg w-80 lg:w-80 flex flex-col justify-around items-center px-10 lg:px-12 h-[478px] my-9 transform shadow-2xl pt-5 transition duration-300 hover:scale-105' key={key}>
-        <a href={link}>
-        
+    <div className='rounded-lg w-80 lg:w-80 flex flex-col justify-around items-center px-12 py-5 lg:px-12 h-[478px] my-9 transform shadow-2xl pt-5 transition duration-300 hover:scale-105' key={key}>
+        <a href={amazonListLink}>        
             <Image
                 className='rounded'
                 src={src}
@@ -33,9 +46,11 @@ export default function GiftCard ({ src, title, description, link, key }: {src: 
                 aria-label="Imagem vela aromática."
                 loading="lazy"                
             />
-        
-        <span className='text-base text-center py-2'>{title}</span>
-        <span className='text-sm text-justify'>{description}</span> 
+        <div className='flex flex-col gap-y-5 items-center'>
+            <span className='text-base text-center py-2'>{title}</span>
+            <span className='text-sm text-justify'>{description}</span> 
+            <span className='text-sm text-justify'>Valor sugerido: R$ {price}</span>
+        </div>        
         </a>
         <button onClick={()=>copyToClipboard('09f42afe-e5df-45de-8d95-7e908b2e9e70')} className='text-center rounded-3xl bg-mossGreen w-[12.31rem] h-[2.06rem] lg:w-[14.31rem] lg:h-[2.75rem]'>
             {isCopied? 'Chave pix copiada!' : 'Copiar chave pix'}
