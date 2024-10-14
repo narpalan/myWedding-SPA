b54a740d7d0805626466f3713907882e1994ddc0
narpalan <thiagofumega@gmail.com>
2024-10-14 20:03:23 -0300

    :sparkles: feat: adds a word... whats the point of this commit? Well... adds final instructions text.

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -30,7 +30,7 @@ const Form = (): React.JSX.Element => {
             {submitted && !formError
                 ? <Msg />
                 : <form action={handleForm} className="flex flex-col mt-[1.31rem] text-justify items-center px-5">
-                    <p className='indent-8'>Para garantir a entrada, <span className='font-bold'>CADA CONVIDADO</span> deve preencher o formulário abaixo.</p>                    
+                    <p className='indent-8'>Para garantir a entrada, <span className='font-bold'>CADA CONVIDADO</span> deve preencher o formulário abaixo, <span className='font-bold'>SEPARADAMENTE!</span></p>                    
                     <p className='indent-8'>Ainda que o nome esteja na lista (cadastrado pelo formulário) é <span className='font-bold'>INDISPENSÁVEL</span> a apresentação do convite individual na entrada.</p>
                     <div className='flex flex-col lg:flex-row lg:gap-x-20 pt-5'>
                         {formFields.map((field, index) => (                            
