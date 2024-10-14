944ae84ffdead00ae78d016db4b229000f4d48eb
narpalan <thiagofumega@gmail.com>
2024-10-14 00:35:15 -0300

    :sparkles: feat: adds linkeding link on footer icons.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = (): React.JSX.Element => {
     return (
         <footer className="flex flex-row flex-wrap h-2 my-3 col-span-4 lg:col-span-12 justify-around items-center">
-            <p className="lg:w-[547px] pb-3 text-black lg:text-small text-center leading-normal font-normal">Feito com muito &#x2764; pelos &#x1F935; &#x1F470;</p>                        
+            <p className="lg:w-[547px] pb-3 text-black lg:text-small text-center leading-normal font-normal">Feito com muito &#x2764; pelos <a href='https://www.linkedin.com/in/thiago-fumega/'>&#x1F935;</a><a href='https://www.linkedin.com/in/thaina-gomes-macedo/'>&#x1F470;</a></p>                        
         </footer>
     );
 }
